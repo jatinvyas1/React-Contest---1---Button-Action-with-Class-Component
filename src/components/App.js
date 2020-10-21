e66463b527d9e91fe9f79d6d13ea035eb80aa6c1
jatinvyas1 <jatin.vyas@outlook.com>
2020-10-21 20:26:28 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,23 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			showPara:false,
+		};
+		this._showPara = this._showPara.bind(this);
 	};
+	_showPara(){
+		this.setState({
+			showPara:true,
+		});
+	}
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={this._showPara()}></button>
+				{this.state.showPara?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
     		</div>
     	);
     }
