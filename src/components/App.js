f945d950c404ac53835c556e5dfa03f95e5476fa
jatinvyas1 <jatin.vyas@outlook.com>
2020-10-21 20:35:24 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			showPara:false,
-		};
-		this._showPara = this._showPara.bind(this);
-	};
-	_showPara(){
-		this.setState({
-			showPara:true,
-		});
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showParagraph: false
+    };
+    this._showPara = this._showPara.bind(this);
+  }
+  _showPara() {
+    this.setState({
+      showParagraph: true
+    });
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-				<button id="click" onClick={this._showPara()}></button>
-				{this.state.showPara?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={this._showPara}></button>
+        {this.state.showParagraph ? 
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+         : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
